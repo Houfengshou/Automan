db3ca9bc761b60e51501e3ea8dd04097e052dd76
Jhashen <13282695985@139.com>
2026-09-11 22:20:37 +0800

    CNv3

--- CN/ProcessCN.m
+++ CN/ProcessCN.m
@@ -1,6 +1,7 @@
 function ProcessCN(dr, times)
     R = 0.02;
-    N = floor(R/dr); % 区间数; 节点数 = 区间数 + 1
+    N = max(1,round(R/dr)); % 区间数; 节点数 = 区间数 + 1
+    dr = R/N;
     dt = 1/times;
 
     hT = 25; %对流换热
@@ -16,12 +17,13 @@ function ProcessCN(dr, times)
     TSettings = interp1(time_points, t_setting, TimePoints, "pchip");
     CSettings = interp1(time_points, c_setting, TimePoints, "pchip");
 
+    r = (0:N)'*dr;
     T = 28.*ones(N + 1,1);
     C = 2.55.*ones(N + 1,1);
     V = zeros(N + 1,1);
     V(1,1) = dr^2/8;
     for index = 2:N
-        V(index,1) = index*dr^2;
+        V(index,1) = (index-1)*dr^2;
     end
     V(N+1,1) = (R^2 - (R-dr/2)^2)/2;
 
